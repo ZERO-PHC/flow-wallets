c99c3d6fafa10f935230bd191f04b87a0524e76c
FelipeRibeiroLabs <frlabsmd@gmail.com>
2022-08-19 21:57:07 -0300

    styledMerged

--- src/components/ToggleColorMode.jsx
+++ src/components/ToggleColorMode.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Button } from '@chakra-ui/react'
 import { useColorMode } from '@chakra-ui/react'
-import { MoonIcon, SunIcon } from '@chakra-ui/icons'
+import { Icon } from '@iconify/react';
 
 export default function ToggleColorMode() {
     const {colorMode, toggleColorMode} = useColorMode()
   return (
     <Button onClick={() => toggleColorMode()} m="1rem" variant="toggle">
-        {colorMode === 'light' ? <MoonIcon w={6} h={6} /> : <SunIcon  w={6} h={6} />}
+        {colorMode === 'light' ? <Icon icon="bi:moon-fill" width="20" /> : <Icon icon="charm:sun" width="20" />}
     </Button>
   )
 }
