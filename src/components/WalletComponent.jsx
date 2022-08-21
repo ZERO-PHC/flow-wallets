ed38dfa5653a04ccda3d3c2e0814ac44987c86c5
Alan Dega <zero@cyphers.gg>
2022-08-21 13:44:19 -0500

    updated styles

--- src/components/WalletComponent.jsx
+++ src/components/WalletComponent.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import { Icon, IconButton, Container, Divider, Flex } from "@chakra-ui/react";
 import WalletSpecs from "./WalletSpecs";
 import WalletInfoComp from "./WalletInfoComp";
+// import useDialog from the providers folder
+import { useDialog } from "../providers/DialogProvider";
 
 import { useRouter } from "next/router";
 
 export const WalletComponent = ({ handleAction, wallet }) => {
   const router = useRouter();
+  const { setOpenSearch } = useDialog()
+
   return (
     <Container
       boxShadow={0}
-      onClick={() => handleAction("wallet", wallet.id, router)}
+      onClick={() => handleAction("wallet", wallet.id, router, setOpenSearch)}
       display="flex"
       height={"auto"}
     >
