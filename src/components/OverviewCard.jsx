fa8bbe7b2577b161dcb4e421359ef7f2e4f54a92
FelipeRibeiroLabs <frlabsmd@gmail.com>
2022-10-15 21:53:44 -0300

    changing onclick rule on hero home page

--- src/components/OverviewCard.jsx
+++ src/components/OverviewCard.jsx
@@ -1,19 +1,27 @@
-import { Container, Text, Button } from '@chakra-ui/react'
-import { useRouter } from 'next/router'
-import React from 'react'
-import { useDialog } from '../providers/DialogProvider'
-import { handleAction } from '../utils/generalUtils'
+import { Container, Text, Button } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import React from "react";
+import { useDialog } from "../providers/DialogProvider";
+import { handleAction } from "../utils/generalUtils";
 
 export default function OverviewCard({ title, subtitle, btnText }) {
-  const router = useRouter()
-  const { setOpenSearch } = useDialog()
+  const router = useRouter();
+  const { setOpenSearch } = useDialog();
 
   return (
-    <Container flexDir="column" variant="overview" p={{ sm: "10px", md: "10px 20px", lg: "15px 30px" }} w="auto" _hover={{ transform: "scale(1.05)", cursor: "pointer" }}>
+    <Container
+      flexDir="column"
+      variant="overview"
+      p={{ sm: "10px", md: "10px 20px", lg: "15px 30px" }}
+      w="auto"
+      _hover={{ transform: "scale(1.05)", cursor: "pointer" }}
+      onClick={() => handleAction("guide", "_", router, setOpenSearch, "_", "_", "what-is-a-wallet")}
+    >
       <Text>{title}</Text>
-      <Text display="flex">{subtitle}
-      </Text>
-      <Button onClick={() => handleAction("guide", "_", router, setOpenSearch, "_", "_", "what-is-a-wallet")} variant="simpleBtn" p="4px">{btnText}</Button>
+      <Text display="flex">{subtitle}</Text>
+      <Button variant="simpleBtn" p="4px">
+        {btnText}
+      </Button>
     </Container>
-  )
+  );
 }
